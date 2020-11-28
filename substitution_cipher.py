16870cdc88238766efe491fa4e982597263cdc54
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2020-11-28 14:33:23 +0200

    Substitution cipher crack was added.

---
+++ substitution_cipher.py
@@ -0,0 +1,160 @@
+import random
+import string
+import time
+
+import cipher_common as cc
+
+
+UPPERCASE_LETTERS = bytes(string.ascii_uppercase, encoding='ascii')
+
+
+class Genotype():
+    def __init__(self, genes=None):
+        # if genes is not specified, genes will be random
+        if genes is None:
+            possible_genes = UPPERCASE_LETTERS
+            # shuffle possible genes
+            self.genes = bytes(random.sample(
+                possible_genes, 
+                len(possible_genes)))
+        else:
+            self.genes = genes
+
+
+    def __crossover_gens(self, gen1, gen2, letters):
+        if gen1 not in letters and \
+                gen2 not in letters:
+            return letters.pop()
+        elif gen1 not in letters:
+            return gen2
+        elif gen2 not in letters:
+            return gen1
+        else:
+            return random.choice((gen1,gen2))
+
+
+    def crossover(self, indiv):
+        result_genes = bytes()
+        letters = set(UPPERCASE_LETTERS)
+
+        for i in range(len(self.genes)):
+            new_gen = self.__crossover_gens(
+                indiv.genes[i], self.genes[i], letters.copy())
+            letters.remove(new_gen)
+            result_genes += bytes([new_gen])
+
+        return Genotype(result_genes)
+
+
+    def mutate(self):
+        i, j = random.sample(range(len(self.genes)), 2)
+        x, y = self.genes[i], self.genes[j]
+        self.genes = self.genes[:j] + bytes([x]) + self.genes[j + 1:]
+        self.genes = self.genes[:i] + bytes([y]) + self.genes[i + 1:]
+        return self.genes
+
+
+    def __repr__(self):
+        return str(self.genes)
+
+
+class NaturalSelection():
+    def __init__(self, 
+        ciphertext, popul_num, kill_percent=0.5, mutation_chance=0.1,
+        exp_index=-4.202, enable_logging=False):
+        # chance to mutate
+        self.mutation_chance = mutation_chance
+        # etalon quadgram index for english text
+        self.exp_index = exp_index
+        self.ciphertext = ciphertext
+        self.popul_num = popul_num
+        self.saved_lives_num = self.popul_num - int(self.popul_num * kill_percent)
+        # init population with random genes
+        self.popul = [Genotype() for _ in range(self.popul_num)]
+        # create fitness list for population and init it
+        self.fitness_list = list()
+        self.__update_fitness_list()
+        # counter of generations
+        self.generation = 0
+        self.enable_logging = enable_logging
+
+
+    def translate_cipher(self, key):
+        bts = bytes(self.ciphertext, encoding='ascii')
+        # translate our cipher text by genes of individual
+        key_map = bytes.maketrans(UPPERCASE_LETTERS, key)
+        return bts.translate(key_map)
+
+
+    def fitness(self, indiv):
+        decoded = self.translate_cipher(indiv.genes)
+        index = cc.quadgram_index(decoded.decode())
+        fitness = abs(index - self.exp_index)
+        return fitness
+
+
+    def __update_fitness_list(self):
+        self.fitness_list = list()
+
+        # create a list of fitness values for population
+        for indiv in self.popul:
+            fitness = self.fitness(indiv)
+            self.fitness_list.append(fitness)
+
+
+    def __choose_parents(self):
+        # select two random and uniq parents from population
+        parent1, parent2 = random.sample(self.popul, 2)
+        return parent1, parent2
+
+
+    def __make_selection(self):
+        # sort fitness list and corresponding individuals
+        self.fitness_list, self.popul = zip(
+            *sorted(
+                zip(self.fitness_list, self.popul), 
+                key=lambda x: x[0]))
+        
+        # delete the worst part
+        self.fitness_list = self.fitness_list[:self.saved_lives_num]
+        self.popul = self.popul[:self.saved_lives_num]
+
+
+    def __crossover_populaton(self):
+        new_popul = list(self.popul)
+        # crossover population until max number of individuals reached
+        while len(new_popul) < self.popul_num:
+            parent1, parent2 = self.__choose_parents()
+            new_popul.append(parent1.crossover(parent2))
+
+        self.popul = new_popul
+
+
+    def __mutate_popul(self):
+        for indiv in self.popul:
+            if random.choices(
+                    [True, False], 
+                    (self.mutation_chance, 1 - self.mutation_chance))[0]:
+                indiv.mutate()
+
+
+    def print_stats(self):
+        print('Generation #', self.generation, 
+                'Best fitness:', self.fitness_list[0])
+
+
+    def solve(self, eps=0.05):
+        start = time.time()
+
+        while self.fitness_list[0] >= eps: 
+            self.__update_fitness_list()
+            self.__make_selection()
+            self.__crossover_populaton()
+            self.__mutate_popul()
+            self.generation += 1
+            self.print_stats()
+
+        key = self.popul[0].genes
+        text = self.translate_cipher(key)
+        print('Runtime:', time.time() - start)
+        return text, key
